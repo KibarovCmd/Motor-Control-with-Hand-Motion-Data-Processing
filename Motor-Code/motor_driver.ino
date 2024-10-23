066f3be73911c4388f49493aaa9cf8cf0320f290
İskender Musaoğlu <117196657+KibarovCmd@users.noreply.github.com>
2024-10-23 20:23:47 +0300

    Update motor_driver.ino

--- Motor-Code/motor_driver.ino
+++ Motor-Code/motor_driver.ino
@@ -31,12 +31,7 @@ void loop() {
     if (data < 30) data = 30;
 
     data = map(data, 30, 240, 0, 180);
-    if(data > 0 && data <= 30) sg90.write(0);
-    if(data > 30 && data <= 60) sg90.write(30);
-    if(data > 60 && data <= 90) sg90.write(45);
-    if(data > 90 && data <= 120) sg90.write(90);
-    if(data > 120 && data <= 150) sg90.write(135);
-    if(data > 150 && data <= 180) sg90.write(180);
+    sg90.write(data);
     //Serial.println(data);
   }
 }
